bfca31eb31ead837adb737412aa87fb9aaafb456
kaito1123 <78860399+kaito1123@users.noreply.github.com>
2023-03-13 10:47:10 +0900

    Add files via upload

---
+++ task2/Form1.Designer.vb
@@ -0,0 +1,308 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown3 = New System.Windows.Forms.NumericUpDown()
+        Me.NumericUpDown4 = New System.Windows.Forms.NumericUpDown()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"男性", "女性"})
+        Me.ComboBox1.Location = New System.Drawing.Point(139, 274)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 32)
+        Me.ComboBox1.TabIndex = 2
+        Me.ComboBox1.Text = "男性"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.RadioButton3)
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Location = New System.Drawing.Point(59, 44)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(560, 166)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "計算式"
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(30, 111)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(288, 28)
+        Me.RadioButton3.TabIndex = 2
+        Me.RadioButton3.Text = "国立スポーツ科学センター"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(236, 44)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(269, 28)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.Text = "国立健康・栄養研究所"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Location = New System.Drawing.Point(30, 44)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(161, 28)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "厚生労働省"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("MS UI Gothic", 9.0!)
+        Me.Label1.Location = New System.Drawing.Point(55, 277)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 24)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "性別"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(55, 359)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(58, 24)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "体重"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(55, 429)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(106, 24)
+        Me.Label3.TabIndex = 15
+        Me.Label3.Text = "体脂肪率"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(332, 280)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(58, 24)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "年齢"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(278, 354)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(32, 24)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "kg"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(311, 429)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(22, 24)
+        Me.Label6.TabIndex = 16
+        Me.Label6.Text = "%"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(380, 354)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(58, 24)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "身長"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(579, 354)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(40, 24)
+        Me.Label8.TabIndex = 14
+        Me.Label8.Text = "cm"
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.DecimalPlaces = 1
+        Me.NumericUpDown1.Increment = New Decimal(New Integer() {1, 0, 0, 65536})
+        Me.NumericUpDown1.Location = New System.Drawing.Point(139, 352)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(120, 31)
+        Me.NumericUpDown1.TabIndex = 4
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.DecimalPlaces = 1
+        Me.NumericUpDown2.Increment = New Decimal(New Integer() {1, 0, 0, 65536})
+        Me.NumericUpDown2.Location = New System.Drawing.Point(166, 429)
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(120, 31)
+        Me.NumericUpDown2.TabIndex = 6
+        '
+        'NumericUpDown3
+        '
+        Me.NumericUpDown3.Cursor = System.Windows.Forms.Cursors.Default
+        Me.NumericUpDown3.Location = New System.Drawing.Point(410, 280)
+        Me.NumericUpDown3.Name = "NumericUpDown3"
+        Me.NumericUpDown3.Size = New System.Drawing.Size(120, 31)
+        Me.NumericUpDown3.TabIndex = 3
+        '
+        'NumericUpDown4
+        '
+        Me.NumericUpDown4.DecimalPlaces = 1
+        Me.NumericUpDown4.Increment = New Decimal(New Integer() {1, 0, 0, 65536})
+        Me.NumericUpDown4.Location = New System.Drawing.Point(444, 357)
+        Me.NumericUpDown4.Maximum = New Decimal(New Integer() {250, 0, 0, 0})
+        Me.NumericUpDown4.Name = "NumericUpDown4"
+        Me.NumericUpDown4.Size = New System.Drawing.Size(120, 31)
+        Me.NumericUpDown4.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(59, 528)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(104, 44)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "計算"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(208, 541)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(136, 31)
+        Me.TextBox1.TabIndex = 8
+        Me.TextBox1.Text = "0"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(204, 495)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(106, 24)
+        Me.Label9.TabIndex = 17
+        Me.Label9.Text = "基礎代謝"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(350, 544)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(49, 24)
+        Me.Label10.TabIndex = 18
+        Me.Label10.Text = "kcal"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(700, 627)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.NumericUpDown4)
+        Me.Controls.Add(Me.NumericUpDown3)
+        Me.Controls.Add(Me.NumericUpDown2)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Name = "Form1"
+        Me.Text = "基礎代謝計算機"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents NumericUpDown2 As NumericUpDown
+    Friend WithEvents NumericUpDown3 As NumericUpDown
+    Friend WithEvents NumericUpDown4 As NumericUpDown
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+End Class
---
+++ task2/Form1.vb
@@ -0,0 +1,220 @@
+﻿Public Class Form1
+
+    Dim gendar As String = "男性"
+    Dim age As Integer = 0
+    Dim weight As Double = 0
+    Dim stature As Double = 0  '身長
+    Dim fatPar As Double = 0
+    Dim metabolism As Double
+    Dim result As Double
+
+    '厚生労働省の計算式
+    Private Function koserodosyo(gendar As String, age As Integer, weight As Double) As Double
+        Me.TextBox1.Text = ""
+
+        gendar = CStr(Me.ComboBox1.SelectedItem)
+        age = CInt(Me.NumericUpDown3.Value)
+        weight = CDbl(Me.NumericUpDown1.Value)
+
+        If age <= 0 AndAlso weight <= 0 Then
+            message()
+        End If
+
+        If age >= 1 AndAlso age <= 2 Then
+            If gendar = "男性" Then
+                metabolism = 61.0 * weight
+            ElseIf gendar = "女性" Then
+                metabolism = 59.7 * weight
+            End If
+        End If
+
+        If age >= 3 AndAlso age <= 5 Then
+            If gendar = "男性" Then
+                metabolism = 54.8 * weight
+            ElseIf gendar = "女性" Then
+                metabolism = 52.2 * weight
+            End If
+        End If
+
+        If age >= 6 AndAlso age <= 7 Then
+            If gendar = "男性" Then
+                metabolism = 44.3 * weight
+            ElseIf gendar = "女性" Then
+                metabolism = 41.9 * weight
+            End If
+        End If
+
+        If age >= 8 AndAlso age <= 9 Then
+            If gendar = "男性" Then
+                metabolism = 40.8 * weight
+            ElseIf gendar = "女性" Then
+                metabolism = 38.3 * weight
+            End If
+        End If
+
+        If age >= 10 AndAlso age <= 11 Then
+            If gendar = "男性" Then
+                metabolism = 37.4 * weight
+            ElseIf gendar = "女性" Then
+                metabolism = 34.8 * weight
+            End If
+        End If
+
+        If age >= 12 AndAlso age <= 14 Then
+            If gendar = "男性" Then
+                metabolism = 31.0 * weight
+            ElseIf gendar = "女性" Then
+                metabolism = 29.6 * weight
+            End If
+        End If
+
+        If age >= 15 AndAlso age <= 17 Then
+            If gendar = "男性" Then
+                metabolism = 27.0 * weight
+            ElseIf gendar = "女性" Then
+                metabolism = 25.3 * weight
+            End If
+        End If
+
+        If age >= 18 AndAlso age <= 29 Then
+            If gendar = "男性" Then
+                metabolism = 24.0 * weight
+            ElseIf gendar = "女性" Then
+                metabolism = 22.1 * weight
+            End If
+        End If
+
+        If age >= 30 AndAlso age <= 49 Then
+            If gendar = "男性" Then
+                metabolism = 22.3 * weight
+            ElseIf gendar = "女性" Then
+                metabolism = 21.7 * weight
+            End If
+        End If
+
+        If age >= 50 AndAlso age <= 69 Then
+            If gendar = "男性" Then
+                metabolism = 21.5 * weight
+            ElseIf gendar = "女性" Then
+                metabolism = 20.7 * weight
+            End If
+        End If
+
+        If age >= 70 Then
+            If gendar = "男性" Then
+                metabolism = 21.5 * weight
+            ElseIf gendar = "女性" Then
+                metabolism = 20.7 * weight
+            End If
+        End If
+
+        result = Math.Round(metabolism, 0, MidpointRounding.AwayFromZero)
+        Return result
+
+    End Function
+
+
+    '国立健康・栄養研究所の計算式
+    Private Function kenkoEyo(gendar As String, age As Integer, weight As Double, stature As Double) As Double
+        Me.TextBox1.Text = ""
+
+        gendar = CStr(Me.ComboBox1.SelectedItem)
+        age = CInt(Me.NumericUpDown3.Value)
+        weight = CDbl(Me.NumericUpDown1.Value)
+        stature = CDbl(Me.NumericUpDown4.Value)
+        Dim gendarNumber As Double
+
+        If age <= 0 AndAlso weight <= 0 AndAlso stature <= 0 Then
+            message()
+            result = 0
+        Else
+            If gendar = "男性" Then
+                gendarNumber = 0.5473 * 1
+            ElseIf gendar = "女性" Then
+                gendarNumber = 0.5473 * 2
+            End If
+            metabolism = ((0.1238 + (0.0481 * weight) + (0.0234 * stature) - (0.0138 * age) - gendarNumber * 1)) * 1000 / 4.186
+
+            result = Math.Round(metabolism, 0, MidpointRounding.AwayFromZero)
+        End If
+
+        Return result
+
+    End Function
+
+
+    '国立スポーツ科学センターの計算式
+    Private Function sports(weight As Double, fatPar As Double) As Double
+        Me.TextBox1.Text = ""
+
+        weight = CDbl(Me.NumericUpDown1.Value)
+        fatPar = CDbl(Me.NumericUpDown2.Value)
+        Dim ffm As Double
+
+        If weight <= 0 AndAlso fatPar <= 0 Then
+            message()
+        End If
+
+        ffm = weight - (weight * (fatPar / 100))
+        metabolism = 28.5 * ffm
+
+        result = Math.Round(metabolism, 0, MidpointRounding.AwayFromZero)
+        Return result
+
+    End Function
+
+
+    '選択可否の切り替え
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        Me.TextBox1.Text = ""
+
+        ComboBox1.Enabled = True
+        NumericUpDown1.Enabled = True
+        NumericUpDown2.Enabled = False
+        NumericUpDown3.Enabled = True
+        NumericUpDown4.Enabled = False
+    End Sub
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        Me.TextBox1.Text = ""
+
+        ComboBox1.Enabled = True
+        NumericUpDown1.Enabled = True
+        NumericUpDown2.Enabled = False
+        NumericUpDown3.Enabled = True
+        NumericUpDown4.Enabled = True
+    End Sub
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+        Me.TextBox1.Text = ""
+
+        ComboBox1.Enabled = False
+        NumericUpDown1.Enabled = True
+        NumericUpDown2.Enabled = True
+        NumericUpDown3.Enabled = False
+        NumericUpDown4.Enabled = False
+    End Sub
+
+
+    '基礎代謝計算の結果の出力
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If RadioButton1.Checked Then
+            koserodosyo(gendar, age, weight)
+        ElseIf RadioButton2.Checked Then
+            kenkoEyo(gendar, age, weight, stature)
+        ElseIf RadioButton3.Checked Then
+            sports(weight, fatPar)
+        End If
+        Me.TextBox1.Text = CStr(result)
+    End Sub
+
+
+    'エラーメッセージ
+    Private Sub message()
+        MessageBox.Show("値が正しくありません。",
+                "エラー",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error
+                )
+    End Sub
+
+
+End Class
